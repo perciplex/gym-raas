f56eb0a2f5425ace3c77c61d7e024e76aaf21626
philzook58 <philzook58@gmail.com>
2019-10-24 22:11:00 -0400

    test drvier code

---
+++ tests/run_pend.py
@@ -0,0 +1,21 @@
+import gym
+import gym_raas
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+env = gym.make('raaspendulum-v0')
+env.reset()
+obs = []
+for i in range(50):
+    observation, reward, done, info = env.step(1)
+    time.sleep(0.01)
+    obs.append(obs)
+for i in range(50):
+    observation, reward, done, info = env.step(-1)
+    time.sleep(0.01)
+    obs.append(obs)
+obs = np.array(obs)
+plt.plot(obs[:,0])
+plt.plot(obs[:,1])
+plt.plot(obs[:,2])
+plt.savefig('books_read.png')
