1f753a2643db8dcd7c8216176ec307bd76891462
Your Name <you@example.com>
2019-10-25 03:56:32 +0100

    made plot

--- tests/run_pend.py
+++ tests/run_pend.py
@@ -9,13 +9,16 @@ obs = []
 for i in range(50):
     observation, reward, done, info = env.step([1])
     time.sleep(0.01)
-    obs.append(obs)
+    obs.append(observation)
+
 for i in range(50):
     observation, reward, done, info = env.step([-1])
     time.sleep(0.01)
-    obs.append(obs)
+    obs.append(observation)
+
 obs = np.array(obs)
 plt.plot(obs[:,0])
 plt.plot(obs[:,1])
 plt.plot(obs[:,2])
 plt.savefig('books_read.png')
+
