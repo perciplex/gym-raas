77c13fe835a51507798a4f63f52683d255c5d28e
Ben <ishmandoo@gmail.com>
2020-08-19 21:32:47 -0400

    registered correct env name

--- gym_raas/__init__.py
+++ gym_raas/__init__.py
@@ -1,3 +1,7 @@
 from gym.envs.registration import register
 
+# Register the RaaS pendulum env
+register(id="RaasPendulum-v0", entry_point="gym_raas.envs:PendulumEnv")
+
+# Register the legacy name
 register(id="raaspendulum-v0", entry_point="gym_raas.envs:PendulumEnv")
